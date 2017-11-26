43cb241c4b697820203719ee9a190c5d1f4c3b0f
preskomilev <preslav.k.milev@gmail.com>
2017-11-27 00:13:39 +0200

    Replace new database file

--- project.sql
+++ project.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.1.14
+-- version 4.5.1
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 24, 2015 at 06:33 AM
--- Server version: 5.6.19
--- PHP Version: 5.5.12
+-- Generation Time: 
+-- Версия на сървъра: 10.1.13-MariaDB
+-- PHP Version: 5.6.21
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -14,7 +14,7 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `project`
@@ -23,62 +23,51 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `admin`
+-- Структура на таблица `admin`
 --
 
-CREATE TABLE IF NOT EXISTS `admin` (
-  `email` varchar(50) NOT NULL,
+CREATE TABLE `admin` (
+  `faculty_num` varchar(225) DEFAULT NULL,
   `password` varchar(500) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `admin`
+-- Схема на данните от таблица `admin`
 --
 
-INSERT INTO `admin` (`email`, `password`) VALUES
-('sunnygkp10@gmail.com', '123456');
+INSERT INTO `admin` (`faculty_num`, `password`) VALUES
+('admin', 'admin');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `answer`
+-- Структура на таблица `answer`
 --
 
-CREATE TABLE IF NOT EXISTS `answer` (
+CREATE TABLE `answer` (
   `qid` text NOT NULL,
   `ansid` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `answer`
+-- Схема на данните от таблица `answer`
 --
 
 INSERT INTO `answer` (`qid`, `ansid`) VALUES
-('55892169bf6a7', '55892169d2efc'),
-('5589216a3646e', '5589216a48722'),
 ('558922117fcef', '5589221195248'),
 ('55892211e44d5', '55892211f1fa7'),
 ('558922894c453', '558922895ea0a'),
 ('558922899ccaa', '55892289aa7cf'),
-('558923538f48d', '558923539a46c'),
-('55892353f05c4', '55892354051be'),
-('558973f4389ac', '558973f462e61'),
-('558973f4c46f2', '558973f4d4abe'),
-('558973f51600d', '558973f526fc5'),
-('558973f55d269', '558973f57af07'),
-('558973f5abb1a', '558973f5e764a'),
-('5589751a63091', '5589751a81bf4'),
-('5589751ad32b8', '5589751adbdbd'),
-('5589751b304ef', '5589751b3b04d'),
-('5589751b749c9', '5589751b9a98c');
+('5a1b3397e3d81', '5a1b3397e4305'),
+('5a1b3397e83b2', '5a1b3397e8af2');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `feedback`
+-- Структура на таблица `feedback`
 --
 
-CREATE TABLE IF NOT EXISTS `feedback` (
+CREATE TABLE `feedback` (
   `id` text NOT NULL,
   `name` varchar(50) NOT NULL,
   `email` varchar(50) NOT NULL,
@@ -89,7 +78,7 @@ CREATE TABLE IF NOT EXISTS `feedback` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `feedback`
+-- Схема на данните от таблица `feedback`
 --
 
 INSERT INTO `feedback` (`id`, `name`, `email`, `subject`, `feedback`, `date`, `time`) VALUES
@@ -103,11 +92,11 @@ INSERT INTO `feedback` (`id`, `name`, `email`, `subject`, `feedback`, `date`, `t
 -- --------------------------------------------------------
 
 --
--- Table structure for table `history`
+-- Структура на таблица `history`
 --
 
-CREATE TABLE IF NOT EXISTS `history` (
-  `email` varchar(50) NOT NULL,
+CREATE TABLE `history` (
+  `faculty_num` varchar(225) DEFAULT NULL,
   `eid` text NOT NULL,
   `score` int(11) NOT NULL,
   `level` int(11) NOT NULL,
@@ -117,44 +106,29 @@ CREATE TABLE IF NOT EXISTS `history` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `history`
+-- Схема на данните от таблица `history`
 --
 
-INSERT INTO `history` (`email`, `eid`, `score`, `level`, `sahi`, `wrong`, `date`) VALUES
-('sunnygkp10@gmail.com', '558921841f1ec', 4, 2, 2, 0, '2015-06-23 09:31:26'),
-('sunnygkp10@gmail.com', '558920ff906b8', 4, 2, 2, 0, '2015-06-23 13:32:09'),
-('avantika420@gmail.com', '558921841f1ec', 4, 2, 2, 0, '2015-06-23 14:33:04'),
-('avantika420@gmail.com', '5589222f16b93', 4, 2, 2, 0, '2015-06-23 14:49:39'),
-('sunnygkp10@gmail.com', '5589741f9ed52', 4, 5, 3, 2, '2015-06-23 15:07:16'),
-('mi5@hollywood.com', '5589222f16b93', 4, 2, 2, 0, '2015-06-23 15:12:56'),
-('nik1@gmail.com', '558921841f1ec', 1, 2, 1, 1, '2015-06-23 16:11:50'),
-('sunnygkp10@gmail.com', '5589222f16b93', 1, 2, 1, 1, '2015-06-24 03:22:38');
+INSERT INTO `history` (`faculty_num`, `eid`, `score`, `level`, `sahi`, `wrong`, `date`) VALUES
+('61462138', '5589222f16b93', 4, 2, 2, 0, '2017-11-26 21:55:28');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `options`
+-- Структура на таблица `options`
 --
 
-CREATE TABLE IF NOT EXISTS `options` (
+CREATE TABLE `options` (
   `qid` varchar(50) NOT NULL,
   `option` varchar(5000) NOT NULL,
   `optionid` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `options`
+-- Схема на данните от таблица `options`
 --
 
 INSERT INTO `options` (`qid`, `option`, `optionid`) VALUES
-('55892169bf6a7', 'usermod', '55892169d2efc'),
-('55892169bf6a7', 'useradd', '55892169d2f05'),
-('55892169bf6a7', 'useralter', '55892169d2f09'),
-('55892169bf6a7', 'groupmod', '55892169d2f0c'),
-('5589216a3646e', '751', '5589216a48713'),
-('5589216a3646e', '752', '5589216a4871a'),
-('5589216a3646e', '754', '5589216a4871f'),
-('5589216a3646e', '755', '5589216a48722'),
 ('558922117fcef', 'echo', '5589221195248'),
 ('558922117fcef', 'print', '558922119525a'),
 ('558922117fcef', 'printf', '5589221195265'),
@@ -171,59 +145,22 @@ INSERT INTO `options` (`qid`, `option`, `optionid`) VALUES
 ('558922899ccaa', 'cin', '55892289aa7df'),
 ('558922899ccaa', 'print', '55892289aa7eb'),
 ('558922899ccaa', 'printf', '55892289aa7f5'),
-('558923538f48d', '255.0.0.0', '558923539a46c'),
-('558923538f48d', '255.255.255.0', '558923539a480'),
-('558923538f48d', '255.255.0.0', '558923539a48b'),
-('558923538f48d', 'none of these', '558923539a495'),
-('55892353f05c4', '192.168.1.100', '5589235405192'),
-('55892353f05c4', '172.168.16.2', '55892354051a3'),
-('55892353f05c4', '10.0.0.0.1', '55892354051b4'),
-('55892353f05c4', '11.11.11.11', '55892354051be'),
-('558973f4389ac', 'containing root file-system required during bootup', '558973f462e44'),
-('558973f4389ac', ' Contains only scripts to be executed during bootup', '558973f462e56'),
-('558973f4389ac', ' Contains root-file system and drivers required to be preloaded during bootup', '558973f462e61'),
-('558973f4389ac', 'None of the above', '558973f462e6b'),
-('558973f4c46f2', 'Kernel', '558973f4d4abe'),
-('558973f4c46f2', 'Shell', '558973f4d4acf'),
-('558973f4c46f2', 'Commands', '558973f4d4ad9'),
-('558973f4c46f2', 'Script', '558973f4d4ae3'),
-('558973f51600d', 'Boot Loading', '558973f526f9d'),
-('558973f51600d', ' Boot Record', '558973f526fb9'),
-('558973f51600d', ' Boot Strapping', '558973f526fc5'),
-('558973f51600d', ' Booting', '558973f526fce'),
-('558973f55d269', ' Quick boot', '558973f57aef1'),
-('558973f55d269', 'Cold boot', '558973f57af07'),
-('558973f55d269', ' Hot boot', '558973f57af17'),
-('558973f55d269', ' Fast boot', '558973f57af27'),
-('558973f5abb1a', 'bash', '558973f5e7623'),
-('558973f5abb1a', ' Csh', '558973f5e7636'),
-('558973f5abb1a', ' ksh', '558973f5e7640'),
-('558973f5abb1a', ' sh', '558973f5e764a'),
-('5589751a63091', 'q', '5589751a81bd6'),
-('5589751a63091', 'wq', '5589751a81be8'),
-('5589751a63091', ' both (a) and (b)', '5589751a81bf4'),
-('5589751a63091', ' none of the mentioned', '5589751a81bfd'),
-('5589751ad32b8', ' moves screen down one page', '5589751adbdbd'),
-('5589751ad32b8', 'moves screen up one page', '5589751adbdce'),
-('5589751ad32b8', 'moves screen up one line', '5589751adbdd8'),
-('5589751ad32b8', ' moves screen down one line', '5589751adbde2'),
-('5589751b304ef', ' yy', '5589751b3b04d'),
-('5589751b304ef', 'yw', '5589751b3b05e'),
-('5589751b304ef', 'yc', '5589751b3b069'),
-('5589751b304ef', ' none of the mentioned', '5589751b3b073'),
-('5589751b749c9', 'X', '5589751b9a98c'),
-('5589751b749c9', 'x', '5589751b9a9a5'),
-('5589751b749c9', 'D', '5589751b9a9b7'),
-('5589751b749c9', 'd', '5589751b9a9c9'),
-('5589751bd02ec', 'autoindentation is not possible in vi editor', '5589751bdadaa');
+('5a1b3397e3d81', 'a', '5a1b3397e4305'),
+('5a1b3397e3d81', 'b', '5a1b3397e430d'),
+('5a1b3397e3d81', 'c', '5a1b3397e4311'),
+('5a1b3397e3d81', 'd', '5a1b3397e4315'),
+('5a1b3397e83b2', 'a', '5a1b3397e8ae3'),
+('5a1b3397e83b2', 'b', '5a1b3397e8af2'),
+('5a1b3397e83b2', 'c', '5a1b3397e8af7'),
+('5a1b3397e83b2', 'd', '5a1b3397e8afc');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `questions`
+-- Структура на таблица `questions`
 --
 
-CREATE TABLE IF NOT EXISTS `questions` (
+CREATE TABLE `questions` (
   `eid` text NOT NULL,
   `qid` text NOT NULL,
   `qns` text NOT NULL,
@@ -232,36 +169,24 @@ CREATE TABLE IF NOT EXISTS `questions` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `questions`
+-- Схема на данните от таблица `questions`
 --
 
 INSERT INTO `questions` (`eid`, `qid`, `qns`, `choice`, `sn`) VALUES
-('558920ff906b8', '55892169bf6a7', 'what is command for changing user information??', 4, 1),
-('558920ff906b8', '5589216a3646e', 'what is permission for view only for other??', 4, 2),
 ('558921841f1ec', '558922117fcef', 'what is command for print in php??', 4, 1),
 ('558921841f1ec', '55892211e44d5', 'which is a variable of php??', 4, 2),
 ('5589222f16b93', '558922894c453', 'what is correct statement in c++??', 4, 1),
 ('5589222f16b93', '558922899ccaa', 'which command is use for print the output in c++?', 4, 2),
-('558922ec03021', '558923538f48d', 'what is correct mask for A class IP???', 4, 1),
-('558922ec03021', '55892353f05c4', 'which is not a private IP??', 4, 2),
-('55897338a6659', '558973f4389ac', 'On Linux, initrd is a file', 4, 1),
-('55897338a6659', '558973f4c46f2', 'Which is loaded into memory when system is booted?', 4, 2),
-('55897338a6659', '558973f51600d', ' The process of starting up a computer is known as', 4, 3),
-('55897338a6659', '558973f55d269', ' Bootstrapping is also known as', 4, 4),
-('55897338a6659', '558973f5abb1a', 'The shell used for Single user mode shell is:', 4, 5),
-('5589741f9ed52', '5589751a63091', ' Which command is used to close the vi editor?', 4, 1),
-('5589741f9ed52', '5589751ad32b8', ' In vi editor, the key combination CTRL+f', 4, 2),
-('5589741f9ed52', '5589751b304ef', ' Which vi editor command copies the current line of the file?', 4, 3),
-('5589741f9ed52', '5589751b749c9', ' Which command is used to delete the character before the cursor location in vi editor?', 4, 4),
-('5589741f9ed52', '5589751bd02ec', ' Which one of the following statement is true?', 4, 5);
+('5a1b337e2cf95', '5a1b3397e3d81', 'aaa', 4, 1),
+('5a1b337e2cf95', '5a1b3397e83b2', 'bbb', 4, 2);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `quiz`
+-- Структура на таблица `quiz`
 --
 
-CREATE TABLE IF NOT EXISTS `quiz` (
+CREATE TABLE `quiz` (
   `eid` text NOT NULL,
   `title` varchar(100) NOT NULL,
   `sahi` int(11) NOT NULL,
@@ -274,68 +199,63 @@ CREATE TABLE IF NOT EXISTS `quiz` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `quiz`
+-- Схема на данните от таблица `quiz`
 --
 
 INSERT INTO `quiz` (`eid`, `title`, `sahi`, `wrong`, `total`, `time`, `intro`, `tag`, `date`) VALUES
-('558920ff906b8', 'Linux : File Managment', 2, 1, 2, 5, '', 'linux', '2015-06-23 09:03:59'),
 ('558921841f1ec', 'Php Coding', 2, 1, 2, 5, '', 'PHP', '2015-06-23 09:06:12'),
 ('5589222f16b93', 'C++ Coding', 2, 1, 2, 5, '', 'c++', '2015-06-23 09:09:03'),
-('558922ec03021', 'Networking', 2, 1, 2, 5, '', 'networking', '2015-06-23 09:12:12'),
-('55897338a6659', 'Linux:startup', 2, 1, 5, 10, '', 'linux', '2015-06-23 14:54:48'),
-('5589741f9ed52', 'Linux :vi Editor', 2, 1, 5, 10, '', 'linux', '2015-06-23 14:58:39');
+('5a1b334488840', 'Test', 2, 0, 2, 1, 'test', '', '2017-11-26 21:33:56'),
+('5a1b337e2cf95', 'Test', 1, 0, 2, 1, 'test', '', '2017-11-26 21:34:54');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `rank`
+-- Структура на таблица `rank`
 --
 
-CREATE TABLE IF NOT EXISTS `rank` (
-  `email` varchar(50) NOT NULL,
+CREATE TABLE `rank` (
+  `faculty_num` varchar(225) DEFAULT NULL,
   `score` int(11) NOT NULL,
   `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `rank`
+-- Схема на данните от таблица `rank`
 --
 
-INSERT INTO `rank` (`email`, `score`, `time`) VALUES
-('sunnygkp10@gmail.com', 9, '2015-06-24 03:22:38'),
-('avantika420@gmail.com', 8, '2015-06-23 14:49:39'),
-('mi5@hollywood.com', 4, '2015-06-23 15:12:56'),
-('nik1@gmail.com', 1, '2015-06-23 16:11:50');
+INSERT INTO `rank` (`faculty_num`, `score`, `time`) VALUES
+('61462138', 4, '2017-11-26 21:55:28');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `user`
+-- Структура на таблица `user`
 --
 
-CREATE TABLE IF NOT EXISTS `user` (
+CREATE TABLE `user` (
   `name` varchar(50) NOT NULL,
-  `gender` varchar(5) NOT NULL,
-  `college` varchar(100) NOT NULL,
-  `email` varchar(50) NOT NULL,
-  `mob` bigint(20) NOT NULL,
   `password` varchar(50) NOT NULL,
-  PRIMARY KEY (`email`)
+  `faculty_num` varchar(36) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `user`
+-- Схема на данните от таблица `user`
 --
 
-INSERT INTO `user` (`name`, `gender`, `college`, `email`, `mob`, `password`) VALUES
-('Avantika', 'F', 'KNIT sultanpur', 'avantika420@gmail.com', 7785068889, 'e10adc3949ba59abbe56e057f20f883e'),
-('Mark Zukarburg', 'M', 'Stanford', 'ceo@facebook.com', 987654321, 'e10adc3949ba59abbe56e057f20f883e'),
-('Komal', 'F', 'KNIT sultanpur', 'komalpd2011@gmail.com', 7785068889, 'e10adc3949ba59abbe56e057f20f883e'),
-('Tom Cruze', 'M', 'Hollywood', 'mi5@hollywood.com', 7785068889, 'e10adc3949ba59abbe56e057f20f883e'),
-('Netcamp', 'M', 'KNIT sultanpur', 'netcamp@gmail.com', 987654321, 'e10adc3949ba59abbe56e057f20f883e'),
-('Nikunj', 'M', 'XYZ', 'nik1@gmail.com', 987, '202cb962ac59075b964b07152d234b70'),
-('Sunny', 'M', 'KNIT sultanpur', 'sunnygkp10@gmail.com', 7785068889, 'e10adc3949ba59abbe56e057f20f883e'),
-('Vikash', 'M', 'KNIT sultanpur@gmail.com', 'vikash@gmail.com', 7785068889, 'e10adc3949ba59abbe56e057f20f883e');
+INSERT INTO `user` (`name`, `password`, `faculty_num`) VALUES
+('Test', '202cb962ac59075b964b07152d234b70', '61462138'),
+('Test', '202cb962ac59075b964b07152d234b70', 'abv');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`faculty_num`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
